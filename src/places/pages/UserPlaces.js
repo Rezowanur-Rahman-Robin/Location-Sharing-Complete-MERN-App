45d2ee36e7a2379c64ba66a8aba8aa5d1157e787
Robin <robincuetcse@gmail.com>
2020-12-05 05:28:59 +0600

    Frontend has been designed By Robin

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -5,32 +5,34 @@ import "./UserPlaces.css";
 
 function UserPlaces(props) {
 
-    const DUMMY_PLACES=[
+    const DUMMY_PLACES = [
         {
-            id: 'p1',
-            title:'Empire State Building',
-            description:'One of the most famous sky scrapers in the world!',
-            imageUrl:'https://images.unsplash.com/photo-1533929736458-ca588d08c8be?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-            address:'20 w 34 St , London , 10012 Ny',
-            location:{
-                lat: 42.99300841943563,
-                lng: -81.24313478955656
-            },
-            creator: 'u1'
+          id: 'p1',
+          title: 'Empire State Building',
+          description: 'One of the most famous sky scrapers in the world!',
+          imageUrl:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+          address: '20 W 34th St, New York, NY 10001',
+          location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+          },
+          creator: 'u1'
         },
         {
-            id: 'p2',
-            title:'Empire State Building',
-            description:'One of the most famous sky scrapers in the world!',
-            imageUrl:'https://lh5.googleusercontent.com/p/AF1QipMZp1yJ0ziULkwIcSxQLLrNF-YEL1LR7RyUwWEe=w408-h268-k-no',
-            address:'20 w 34 St , London , 10012 Ny',
-            location:{
-                lat: 42.99300841943563,
-                lng: -81.24313478955656
-            },
-            creator: 'u2'
+          id: 'p2',
+          title: 'Emp. State Building',
+          description: 'One of the most famous sky scrapers in the world!',
+          imageUrl:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+          address: '20 W 34th St, New York, NY 10001',
+          location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+          },
+          creator: 'u2'
         }
-    ];
+      ];
     const userId = useParams().userId;
     const loadedPlaces = DUMMY_PLACES.filter(place =>{
         return place.creator===userId;
